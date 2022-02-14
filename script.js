9a5227a209f1d0b8c1f6e60b8a178ca44f93449a
jemx <jemx@mboutrecht.nl>
2022-02-14 13:41:56 +0100

    script.js added. 2 examples added. Rest will be explained in the assignment.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Het img element wordt geselecteerd.
+// En het src attribuut wordt aangepast.
+const photoEl = document.querySelector('div.profile-photo img');
+photoEl.src = 'img/john_doe.png';
+
+// Het h3 element met de klasse name wordt geselecteerd.
+// En de innerlijke html wordt aangepast.
+const nameEl = document.querySelector('h3.name');
+nameEl.innerHTML = 'John Doe';
